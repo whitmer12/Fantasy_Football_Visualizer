5eaa2b04de4c2223026d8a384e2ca7708167ef20
rywh3896 <rywh3896@colorado.edu>
2018-10-18 01:27:10 -0600

    committing project part 2

---
+++ CodeBase/FantasyPlayer.java
@@ -0,0 +1,20 @@
+
+public class FantasyPlayer {
+	
+	private int totalTouchdowns;
+	private int scrimmageYards;
+	private String team;
+	
+	public int getTotalTouchdowns() {return totalTouchdowns;}
+	
+	public int getScrimmageYards() {return scrimmageYards;}
+	
+	public String getTeam() {return team;}
+	
+	public void setTotalTouchdowns(int totalTouchdowns) {this.totalTouchdowns = totalTouchdowns;}
+	
+	public void setScrimmageYards(int scrimmageYards) {this.scrimmageYards = scrimmageYards;}
+	
+	public void setTeam(String team) {this.team = team;}
+
+}
